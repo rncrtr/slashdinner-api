7dc66808317b28ca62b8e351ff38d2e03afc4145
Ryan Carter <me@rncrtr.in>
2017-10-22 00:36:34 +0000

    changed to mlab database

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 export default callback => {
-  let db = mongoose.connect('mongodb://localhost:27017/slashdinner-api',{useMongoClient: true});
+  //let db = mongoose.connect('mongodb://localhost:27017/slashdinner-api',{useMongoClient: true});
+  let db = mongoose.connect('mongodb://rncrtr:Zl8tT13veS5l@ds227555.mlab.com:27555/slashdinner',{useMongoClient: true});
+  
   callback(db);
 }
